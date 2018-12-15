669d3f1d4f671f74b501fe541842ea90ed91e6da
Peter Gorgone <peter@peltronic.com>
2018-12-14 19:25:12 -0800

    Add form to create note

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,13 +1,21 @@
 <template>
     <section>
         <div>
-            <b-button :size="sm" :variant="success">New Note</b-button>
+            <b-button v-if="!isCreateFormVisible" size="sm" variant="success" @click="createNote()">New Note</b-button>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="isCreateFormVisible">
+                <b-form-group id="note-contents-group" label="Contents:" label-for="note-content" description="Enter note contents">
+                    <b-form-input id="note-content" type="text" v-model="createForm.contents" required placeholder="Type the note...">
+                    </b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Submit</b-button>
+                <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
         </div>
         <ul id="list">
                 <li v-for="nobj,cnt in this.notes" v-bind:key="cnt" class="">
                     <span class="contents">{{ nobj.contents }}</span> -
-                    <span v-on:click="handleEdit($event, nobj)" class="clickme_to_edit">Edit</span> |
-                    <span v-on:click="handleDelete($event, nobj)" class="clickme_to_delete">Delete</span>
+                    <span v-on:click="onEdit($event, nobj)" class="clickme_to_edit">Edit</span> |
+                    <span v-on:click="onDelete($event, nobj)" class="clickme_to_delete">Delete</span>
                 </li>
 <!--
             <li class="note">
@@ -37,22 +45,41 @@ export default {
     name: 'Book',
     data () {
         return {
-            notes: store.notes
+            isCreateFormVisible: false,
+            notes: store.notes,
+            createForm: {
+                contents: '',
+            },
         }
     },
     props: {
         msg: String
     },
-    handleEdit (e, nobj) {
-        console.log('edit...');
-        //this.form.textstr = cobj.cstring;
-        //this.updateReader(cobj.cstring);
-    },
-    handleDelete (e, nobj) {
-        console.log('delete...');
-        //this.form.textstr = cobj.cstring;
-        //this.updateReader(cobj.cstring);
-    },
+    methods: {
+        createNote () {
+            console.log('create...');
+            this.isCreateFormVisible = true;
+        },
+        onEdit (e, nobj) {
+            console.log('edit...');
+            //this.form.textstr = cobj.cstring;
+            //this.updateReader(cobj.cstring);
+        },
+        onDelete (e, nobj) {
+            console.log('delete...');
+            //this.form.textstr = cobj.cstring;
+            //this.updateReader(cobj.cstring);
+        },
+        onSubmit (e) {
+            e.preventDefault();
+            alert(JSON.stringify(this.form));
+        },
+        onReset (e) {
+            e.preventDefault();
+            this.createForm.contents = ''; // reset values
+            this.isCreateFormVisible = false; // hide form
+        }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
-
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 new Vue({
   render: h => h(App),
